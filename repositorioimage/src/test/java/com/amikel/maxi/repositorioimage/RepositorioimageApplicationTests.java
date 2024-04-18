8177c20d8a5fd5c596b63f4b03a0e1b950ea5b03
maxloversist19 <maxloversist19@gmail.com>
2024-04-18 14:05:35 -0300

    Inicial commit. Configuração inicial básica

---
+++ repositorioimage/src/test/java/com/amikel/maxi/repositorioimage/RepositorioimageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amikel.maxi.repositorioimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioimageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
